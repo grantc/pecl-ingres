a00dbf7b01ebf102d6d4aa2f2822c2af4ab63901
grantc <grantc@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-09-03 10:02:17 +0000

    Prepare for 2.2.3 release
    git-svn-id: http://svn.php.net/repository/pecl/ingres/trunk@316074 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_ingres.h
+++ php_ingres.h
@@ -31,7 +31,7 @@
 extern zend_module_entry ingres_module_entry;
 #define phpext_ingres_ptr &ingres_module_entry
 
-#define PHP_INGRES_VERSION "2.2.3-dev"
+#define PHP_INGRES_VERSION "2.2.3"
 
 #ifdef PHP_WIN32
 #define PHP_INGRES_API __declspec(dllexport)
